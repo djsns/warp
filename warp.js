f4a32e4332bf788dc6d19c2123157141fae8eb64
djsns <djsns@users.noreply.github.com>
2016-07-29 00:04:02 +0200

    began to design level 2

--- warp.js
+++ warp.js
@@ -9,7 +9,7 @@ const banner = Banner({
 const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 
-const level = levelFactory.createLevelNumber(1);
+const level = levelFactory.createLevelNumber(2);
 const controller = Controller(level.player, level);
 
 level.addResultListener(result => {
