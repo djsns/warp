39202953095585fa66010d227e70708bbd9df22d
djsns <djsns@users.noreply.github.com>
2016-08-08 16:00:59 +0200

    simpler Controller

--- controller.js
+++ controller.js
@@ -6,17 +6,15 @@ function Controller(player, level) {
 
   this.player = player;
   this.level = level;
-  this.keyActions = {
-    keydown : {
-      37 : () => {this.player && this.player.nudgeLeft()},
-      65 : () => {this.player && this.player.nudgeLeft()},
-      38 : () => {this.player && this.player.nudgeUp()},
-      87 : () => {this.player && this.player.nudgeUp()},
-      39 : () => {this.player && this.player.nudgeRight()},
-      68 : () => {this.player && this.player.nudgeRight()},
-      40 : () => {this.player && this.player.nudgeDown()},
-      83 : () => {this.player && this.player.nudgeDown()},
-    },
+  this.keyDownActions = {
+    37 : () => {this.player && this.player.nudgeLeft()},
+    65 : () => {this.player && this.player.nudgeLeft()},
+    38 : () => {this.player && this.player.nudgeUp()},
+    87 : () => {this.player && this.player.nudgeUp()},
+    39 : () => {this.player && this.player.nudgeRight()},
+    68 : () => {this.player && this.player.nudgeRight()},
+    40 : () => {this.player && this.player.nudgeDown()},
+    83 : () => {this.player && this.player.nudgeDown()},
   };
 
   this.handleVisibilityChange = () => {
@@ -25,19 +23,17 @@ function Controller(player, level) {
     else this.level && this.level.startGameLoop(context);
   };
 
-  this.handleKeyEvent = event => {
-    const action = this.keyActions[event.type] && this.keyActions[event.type][event.keyCode];
+  this.handleKeyDown = event => {
+    const action = this.keyDownActions[event.keyCode];
     if(action)
       action.call(this);
   };
 
-  document.addEventListener('keydown', this.handleKeyEvent);
-  document.addEventListener('keyup', this.handleKeyEvent);
+  document.addEventListener('keydown', this.handleKeyDown);
   document.addEventListener('visibilitychange', this.handleVisibilityChange);
 
   this.detach = () => {
-    document.removeEventListener('keydown', this.handleKeyEvent);
-    document.removeEventListener('keyup', this.handleKeyEvent);
+    document.removeEventListener('keydown', this.handleKeyDown);
     document.removeEventListener('visibilitychange', this.handleVisibilityChange);
     this.player = null;
     this.level = null;
